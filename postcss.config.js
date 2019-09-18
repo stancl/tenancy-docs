a4356ce76e535f36881911bdd4fa4e63a5f6e024
Samuel Štancl <samuel.stancl@gmail.com>
2019-09-18 16:27:28 +0200

    Add % to purgecss pattern, delete tailwind.css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./dist/*.html'],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/%]+/g) || []
 })
 
 module.exports = {
