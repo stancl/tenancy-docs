ba394c38cc574cdb96f5b4ba0a6e4835fbef73fa
Samuel Štancl <samuel.stancl@gmail.com>
2019-10-12 17:03:02 +0200

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-      2xl: '1600px',
+      '2xl': '1600px',
     },
     colors: {
       transparent: 'transparent',
