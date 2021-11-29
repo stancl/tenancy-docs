09f6c5d0c0c2c45ffe932e2cda666554ef01ff64
Samuel Štancl <samuel.stancl@gmail.com>
2021-11-29 20:11:10 +0100

    wip

--- source/_assets/js/extsb.js
+++ source/_assets/js/extsb.js
@@ -1,10 +1,25 @@
 import splitbee from '@splitbee/web';
 
-splitbee.init({
-  scriptUrl: "https://tenancyforlaravel.com/bee.js",
-  apiUrl: "https://tenancyforlaravel.com/_hive",
-})
+(function (tag) {
+    if (! tag) {
+        return;
+    }
 
-let tag = document.getElementsByClassName('extsb')[0];
+    let data = tag.dataset;
+    if (! data.token) {
+        return;
+    }
 
-splitbee.user.set({test: 'foo'})
+    splitbee.init({
+        scriptUrl: "https://tenancyforlaravel.com/bee.js",
+        apiUrl: "https://tenancyforlaravel.com/_hive",
+        token: data.token,
+    })
+
+    splitbee.enableCookie();
+    splitbee.user.set({github: data.github});
+    splitbee.track('Purchase', {
+        product: data.product,
+        price: data.price,
+    });
+})(document.getElementById('extsb'));
